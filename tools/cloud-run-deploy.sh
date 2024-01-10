2699fb381a6a54f46e9e9e62d4d3a067d88e33a1
karutselvan <114212898+karutselvan@users.noreply.github.com>
2024-01-10 11:12:22 +0530

    Update cloud-run-deploy.sh

--- tools/cloud-run-deploy.sh
+++ tools/cloud-run-deploy.sh
@@ -1,1 +1,1 @@
-gcloud run deploy cloudrunservice --image us-central1-docker.pkg.dev/robsite-assistant-prod/robsite-assistant-repo/test:latest --region us-central1 --platform managed --allow-unauthenticated
+gcloud run deploy cloudrunservice --image us-central1-docker.pkg.dev/karutselvan-dm-test/cloud-run-source-deploy/chat-assistant:latest --region us-central1 --platform managed --allow-unauthenticated
